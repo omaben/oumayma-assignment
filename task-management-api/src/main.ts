18d57aaca42ebbe046af94303effd53e6c11709c
Oumayma Bendada <oumayma.bendada@gmail.com>
2024-10-24 09:50:02 +0400

    swagger config

--- task-management-api/src/main.ts
+++ task-management-api/src/main.ts
@@ -1,8 +1,24 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
+dotenv.config();
+const PORT = process.env.PORT || 3006;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('Task Management API description')
+    .setVersion('1.0')
+    .addTag('Task Management')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(PORT);
+  
 }
 bootstrap();
